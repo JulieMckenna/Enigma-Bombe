ef7d6a3305a6606a7bd0297c0dcfff95c1ec4827
mckennaj6 <mckennaj6@wit.edu>
2020-06-11 18:12:50 -0400

    making menu

---
+++ Enigma.py
@@ -0,0 +1,24 @@
+#Enigma code
+
+#array for the a
+
+#menu
+#get input message
+plainTestMessage = input("What is the message you would like to input")
+#get settings 
+#plug board
+for x in range(10):
+    print("Plugboard connection: " + x + "/10")
+    OrigLetter = input("What letter would you like to swap?")
+    ChangeLetter = input("What is it being changed to?")
+#rotors
+rotorarray = []
+for x in range(3):
+    rotornum = input("What is the " + x + " you would like to use?")
+    rotorarray.append(rotornum)
+#reflector
+reflector = input("Please choose the reflector")
+
+print("The encoded message is: ")
+
+

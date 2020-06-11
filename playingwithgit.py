a5d702b779b880d3fe469b3e2432303d03c7e2fb
Michael Kearns <kearnsm2@wit.edu>
2020-06-11 13:22:49 -0400

    committing to branch

---
+++ playingwithgit.py
@@ -0,0 +1,13 @@
+#work on pulling, committing, and pushing
+
+
+print("Hi this is Julie")
+#working on making changes on my local computer then pushig to master
+#all done
+
+
+
+# Practicing pulling, committing, and pushing
+
+print("Hi this is Mike")
+print("Now testing branches")

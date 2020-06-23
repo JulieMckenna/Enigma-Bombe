fa9afb1df75ec3f67b5c5a91cb6a1615ac7ada85
mckennaj6 <mckennaj6@wit.edu>
2020-06-23 15:19:22 -0400

    Adding plugboard functionality

--- Enigma.py
+++ Enigma.py
@@ -11,22 +11,43 @@ for x in range(26):
     alphabet[0, int(x)] = chr(charnum)
     alphabet[1, int(x)] = chr(charnum)
     charnum+=1
-for x in range(26):
-    print(alphabet[0,x])
-    print(alphabet[1,x])
+
+def printalphabet(num):
+    for x in range(26):
+        print(alphabet[num,x])
+
+def plugboard(ogletter, swapedletter):
+    print("Plugboard section")
+    print("Swapping letter: " + ogletter + " with: " + swapedletter)
+    #swap the letters
+    ogletterindex = 0
+    swapletterindex = 0
+    #search for the index of letters to swap
+    for x in range(26):
+        if (alphabet[1, x] == ogletter):
+            ogletterindex = x
+            #print(str(ogletterindex))
+        if (alphabet[1, x] == swapedletter):
+            swapletterindex = x
+    #swap the two in the array
+    alphabet[1, ogletterindex] = swapedletter
+    alphabet[1, swapletterindex] = ogletter
+    #prints cipher alphabet to check that the swap worked
+    printalphabet(1)
 
 #menu
 #get input message
-plainTestMessage = input("What is the message you would like to input")
+plainTextMessage = input("What is the message you would like to input")
 #get rid of spaces
 #make uppercase so chars match with the alphabet
 
 #get settings 
 #plug board
 for x in range(10):
-    print("Plugboard connection: " + x + "/10")
-    OrigLetter = input("What letter would you like to swap?")
-    ChangeLetter = input("What is it being changed to?")
+    print("Plugboard connection: " + str(x) + "/10")
+    OrigLetter = input("What letter would you like to swap?").upper()
+    ChangeLetter = input("What is it being changed to?").upper()
+    plugboard(OrigLetter, ChangeLetter)
 #rotors
 rotorarray = []
 for x in range(3):
@@ -36,15 +57,3 @@ for x in range(3):
 reflector = input("Please choose the reflector")
 
 print("The encoded message is: ")
-
-def plugboard(ogletter, swapedletter):
-    print("Plugboard section")
-    print("Swapping letter: " + ogletter + " with: " + swapedletter)
-    #swap the letters
-
-    #search for where the ogletter is (index)
-
-    #serach for where the swapped letter (index)
-
-    #swap the two in the array
-

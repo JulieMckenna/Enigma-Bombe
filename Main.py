77cea7b1238f2e6cfb349afcd9c8b7428ecbb72f
Millert4atwit <Millert4@wit.edu>
2020-07-25 00:16:34 -0400

    gifified the gif

--- Main.py
+++ Main.py
@@ -7,12 +7,37 @@ root = Tk()
 root.title("Enigma")
 # root.geometry("400X400")
 #adding image to window
-image = Image.open("enigma.gif")
-photo = ImageTk.PhotoImage(image)
+#image = Image.open("enigma.gif")
+#photo = ImageTk.PhotoImage(image)
 
-Limage = Label(image=photo)
-Limage.image = photo
-Limage.grid(row=0,column=0,columnspan =6,sticky=W+N)
+
+
+frame = 0
+def gif():
+    while True:
+        try:
+            global photo
+            global frame
+            global label
+            photo = PhotoImage(file = "enigma.gif", format = "gif - {}".format(frame))
+            label.configure(image = photo)
+            frame = frame + 1
+            break
+        except Exception:
+            frame = 1
+            break
+def setBlank():
+    global label
+    global frame
+    while frame <22:
+        time.sleep(.2)
+        root.after(500, lambda: gif())
+        label.update()
+photo = PhotoImage(file = "enigma.gif",)
+label = Label(image = photo)
+label.grid(row = 0, column = 0, columnspan = 6, sticky= W+N)
+animate = Button(root, text = "animate", command = setBlank)
+animate.grid(row= 20, column = 0)
 
 
 # rotor position sliders and displaying on screen

72ac591f86b3f5fee3091cf8f5e99123b02acb64
kearnsm2atwit <55287446+kearnsm2atwit@users.noreply.github.com>
2020-07-24 21:52:15 -0400

    Added third rotor to Bombe

--- bombe.py
+++ bombe.py
@@ -1,16 +1,19 @@
-from Enigma2 import enigma
+from enigma import enigma
 
 #Bombe
 """
 steps of to create - will be simplier than an actual Bombe machine
 1. 2 rotors - know the which rotors but no their position(limited positions(0-10))
 2. add in full possition range
-3. add in 3rd rotos - if we get to it
+3. add in 3rd rotors - if we get to it
+
 """
 
 
 ROTOR_I =   "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
 ROTOR_II =  "AJDKSIRUXBLHWTMCQGZNPYFVOE"
+ROTOR_III = "BDFHJLCPRTXVZNYEIWGAKMUSQO"
+
 ## A comes out of R3 then goes to the reflector
 REFLECTOR_B = "YRUHQSLDPXNGOKMIEBFZCWVJAT"
 REFLECTOR_A = "EDCHIJKLMNOPQRSTUVWZYZABGF"
@@ -18,10 +21,11 @@ REFLECTOR_A = "EDCHIJKLMNOPQRSTUVWZYZABGF"
 #will have multiple enigma machines running at once. One will take setting 1 half of positions
 
 def main():
+    numOfAttempts = 0
     reflector = REFLECTOR_B
     #find possible encryptions first - to then solve with decrption 
-    engimaMachineINPUT = enigma("",[], ROTOR_I, ROTOR_II, reflector, 10, 10, True)
-    engimaMachineOUTPUT = enigma("",[], ROTOR_I, ROTOR_II, reflector, 10, 10, True)
+    engimaMachineINPUT = enigma("",[], ROTOR_I, ROTOR_II, ROTOR_III, reflector, 5, 0, 25, False)
+    engimaMachineOUTPUT = enigma("",[], ROTOR_I, ROTOR_II, ROTOR_III, reflector, 5, 0, 25, False)
 
     inputText = (input("Enter message: ")).upper()
     #adds this to end of the message - to use to solve fo rthe crib
@@ -35,88 +39,99 @@ def main():
 
     #to decrypt throwing weird error not sure why!!!
 
-    inputText = input("Enter the message you would like to decrypt: ").upper()
+    inputText = input("Press enter to run encrypted message through Bombe machine: ").upper()
+    inputText = outputText
     crib="HELLOWORLD"
     for i in range(0, 6):
-        for j in range(0, 25):
-            #makes a machine with the varied settings
-            machine = enigma("",[], ROTOR_I, ROTOR_II, reflector, int(i), int(j), True)
-            outputText = machine.encrypt(inputText)
-            print(outputText)
-            #checks if th last 10 letters of the "decrypted" message = helloworld 
-            if outputText[(len(inputText)-10):] == crib:
-                #means that the correct settings have been found
-                print("Settings have been found!")
-                print("Settings are: Rotor I at offset: "+ str(i) +" Rotor II at offset: "+ str(j))
-                #this is where the insert query
-                #prints the decrypted message
-                print("Decrypted message: " + outputText[0:(len(outputText)-10)]) 
-                return
-            del machine
+        for j in range(0, 26):
+            for k in range(0, 26):
+                numOfAttempts += 1
+                #makes a machine with the varied settings
+                machine = enigma("",[], ROTOR_I, ROTOR_II, ROTOR_III, reflector, int(i), int(j), int(k), False)
+                outputText = machine.encrypt(inputText)
+                #checks if th last 10 letters of the "decrypted" message = helloworld 
+                if outputText[(len(inputText)-10):] == crib:
+                    #means that the correct settings have been found
+                    print("Settings have been found!")
+                    print("Settings are: Rotor I at offset: "+ str(i) +" Rotor II at offset: "+ str(j) + " Rotor III at offset: " + str(k))
+                    #this is where the insert query
+                    #prints the decrypted message
+                    print("Decrypted message: " + outputText[0:(len(outputText)-10)])
+                    print("Number of combinations tried: " + str(numOfAttempts))
+                    return
+                del machine
     for i in range(6,11):
-        for j in range(0, 25):
-            #makes a machine with the varied settings
-            machine = enigma("",[], ROTOR_I, ROTOR_II, reflector, int(i), int(j), True)
-            outputText = machine.encrypt(inputText)
-            print(outputText)
-            #checks if th last 10 letters of the "decrypted" message = helloworld 
-            if outputText[(len(inputText)-10):] == crib:
-                #means that the correct settings have been found
-                print("Settings have been found!")
-                print("Settings are: Rotor I at offset: "+ str(i) +" Rotor II at offset: "+ str(j))
-                #this is where the insert query
-                #prints the decrypted message
-                print("Decrypted message: " + outputText[0:(len(outputText)-10)])
-                return
-            del machine
+        for j in range(0, 26):
+            for k in range(0, 26):
+                numOfAttempts += 1
+                #makes a machine with the varied settings
+                machine = enigma("",[], ROTOR_I, ROTOR_II, ROTOR_III, reflector, int(i), int(j), int(k), False)
+                outputText = machine.encrypt(inputText)
+                #checks if th last 10 letters of the "decrypted" message = helloworld 
+                if outputText[(len(inputText)-10):] == crib:
+                    #means that the correct settings have been found
+                    print("Settings have been found!")
+                    print("Settings are: Rotor I at offset: "+ str(i) +" Rotor II at offset: "+ str(j))
+                    #this is where the insert query
+                    #prints the decrypted message
+                    print("Decrypted message: " + outputText[0:(len(outputText)-10)])
+                    print("Number of combinations tried: " + str(numOfAttempts))
+                    return
+                del machine
     for i in range(11,16):
-        for j in range(0, 25):
-            #makes a machine with the varied settings
-            machine = enigma("",[], ROTOR_I, ROTOR_II, reflector, int(i), int(j), True)
-            outputText = machine.encrypt(inputText)
-            print(outputText)
-            #checks if th last 10 letters of the "decrypted" message = helloworld 
-            if outputText[(len(inputText)-10):] == crib:
-                #means that the correct settings have been found
-                print("Settings have been found!")
-                print("Settings are: Rotor I at offset: "+ str(i) +" Rotor II at offset: "+ str(j)) 
-                #this is where the insert query
-                #prints the decrypted message
-                print("Decrypted message: " + outputText[0:(len(outputText)-10)])
-                return
-            del machine
+        for j in range(0, 26):
+            for k in range(0, 26):
+                numOfAttempts += 1
+                #makes a machine with the varied settings
+                machine = enigma("",[], ROTOR_I, ROTOR_II, ROTOR_III, reflector, int(i), int(j), int(k), False)
+                outputText = machine.encrypt(inputText)
+                #checks if th last 10 letters of the "decrypted" message = helloworld 
+                if outputText[(len(inputText)-10):] == crib:
+                    #means that the correct settings have been found
+                    print("Settings have been found!")
+                    print("Settings are: Rotor I at offset: "+ str(i) +" Rotor II at offset: "+ str(j))
+                    #this is where the insert query
+                    #prints the decrypted message
+                    print("Decrypted message: " + outputText[0:(len(outputText)-10)])
+                    print("Number of combinations tried: " + str(numOfAttempts))
+                    return
+                del machine
     for i in range(16,21):
-        for j in range(0, 25):
-            machine = enigma("",[], ROTOR_I, ROTOR_II, reflector, int(i), int(j), True)
-            outputText = machine.encrypt(inputText)
-            print(outputText)
-            #checks if th last 10 letters of the "decrypted" message = helloworld 
-            if outputText[(len(inputText)-10):] == crib:
-                #means that the correct settings have been found
-                print("Settings have been found!")
-                print("Settings are: Rotor I at offset: "+ str(i) +" Rotor II at offset: "+ str(j))  
-                #this is where the insert query
-                #prints the decrypted message
-                print("Decrypted message: " + outputText[0:(len(outputText)-10)]) 
-                return
-            del machine 
-    for i in range(21,25):
-        for j in range(0, 25):
-            #makes a machine with the varied settings
-            machine = enigma("",[], ROTOR_I, ROTOR_II, reflector, int(i), int(j), True)
-            outputText = machine.encrypt(inputText)
-            print(outputText)
-            #checks if th last 10 letters of the "decrypted" message = helloworld 
-            if outputText[(len(inputText)-10):] == crib:
-                print(outputText[(len(inputText)-10):])
-                #means that the correct settings have been found
-                print("Settings have been found!")
-                print("Settings are: Rotor I at offset: "+ str(i) +" Rotor II at offset: "+ str(j))  
-                #this is where the insert query
-                #prints the decrypted message
-                print("Decrypted message: " + outputText[0:(len(outputText)-10)]) 
-                return
-            del machine  
+        for j in range(0, 26):
+            for k in range(0, 26):
+                numOfAttempts += 1
+                #makes a machine with the varied settings
+                machine = enigma("",[], ROTOR_I, ROTOR_II, ROTOR_III, reflector, int(i), int(j), int(k), False)
+                outputText = machine.encrypt(inputText)
+                #checks if th last 10 letters of the "decrypted" message = helloworld 
+                if outputText[(len(inputText)-10):] == crib:
+                    #means that the correct settings have been found
+                    print("Settings have been found!")
+                    print("Settings are: Rotor I at offset: "+ str(i) +" Rotor II at offset: "+ str(j))
+                    #this is where the insert query
+                    #prints the decrypted message
+                    print("Decrypted message: " + outputText[0:(len(outputText)-10)])
+                    print("Number of combinations tried: " + str(numOfAttempts))
+                    return
+                del machine
+    for i in range(21,26):
+        for j in range(0, 26):
+            for k in range(0, 26):
+                numOfAttempts += 1
+                #makes a machine with the varied settings
+                machine = enigma("",[], ROTOR_I, ROTOR_II, ROTOR_III, reflector, int(i), int(j), int(k), False)
+                outputText = machine.encrypt(inputText)
+                #checks if th last 10 letters of the "decrypted" message = helloworld 
+                if outputText[(len(inputText)-10):] == crib:
+                    #means that the correct settings have been found
+                    print("Settings have been found!")
+                    print("Settings are: Rotor I at offset: "+ str(i) +" Rotor II at offset: "+ str(j))
+                    #this is where the insert query
+                    #prints the decrypted message
+                    print("Decrypted message: " + outputText[0:(len(outputText)-10)])
+                    print("Number of combinations tried: " + str(numOfAttempts))
+                    return
+                del machine 
     print("Nothing found")
 main()
 

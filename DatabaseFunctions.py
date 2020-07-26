ab8dda3cf9b14ef45e4248815c6bfa04d9530693
Chandler Berry <58996908+chandlerberry@users.noreply.github.com>
2020-07-26 16:02:25 -0400

    changed around bombe db functions to work correctly with bombe.py

--- DatabaseFunctions.py
+++ DatabaseFunctions.py
@@ -38,38 +38,23 @@ class CreateDatabases:
     # create the bombe database, this can also be used to reset the database
     def createBombeDB(self):
         # drop tables if they exist before creating them
-        tables = ['PossibleConfig', 'KnownConfig', 'CapturedMsg']
+        tables = ['KnownConfig', 'CapturedMsg']
         for t in tables:
             tbl = self.bombe.cursor()
             tbl.execute('DROP TABLE IF EXISTS ' + t)
             tbl.close()
 
-        # create table script for PossibleConfig table
-        possibleConfig = '''
-        CREATE TABLE PossibleConfig
-        (
-            Day                 INT         PRIMARY KEY NOT NULL,
-            ActiveR1            INT         NOT NULL,
-            Offset1             INT         NOT NULL,
-            ActiveR2            INT         NOT NULL,
-            Offset2             INT         NOT NULL,
-            Reflector           TEXT        NOT NULL
-        );
-        '''
-        # create PossibleConfig table
-        pC = self.bombe.cursor()
-        pC.execute(possibleConfig)
-        pC.close()
-
         # create table script for KnownConfig table
         knownConfig = '''
         CREATE TABLE KnownConfig
         (
-            Day                 INT         PRIMARY KEY NOT NULL,
+            ID                  INT         PRIMARY KEY NOT NULL,
             ActiveR1            INT         NOT NULL,
             Offset1             INT         NOT NULL,
             ActiveR2            INT         NOT NULL,
             Offset2             INT         NOT NULL,
+            ActiveR3            INT         NOT NULL,
+            Offset3             INT         NOT NULL,
             Reflector           TEXT        NOT NULL
         );
         '''
@@ -77,22 +62,6 @@ class CreateDatabases:
         kC = self.bombe.cursor()
         kC.execute(knownConfig)
         kC.close()
-
-        # create table script for CapturedMsg table
-        capturedMsg = '''
-        CREATE TABLE CapturedMsg
-        (
-            MessageID       INT     PRIMARY KEY NOT NULL,
-            EncryptedMsg    TEXT    NOT NULL,
-            IsDecrypted     INT     NOT NULL,
-            DecryptedMsg    TEXT,
-            Day             INT     NOT NULL
-        );
-        '''        
-        # create CapturedMsg table
-        capMsg = self.bombe.cursor()
-        capMsg.execute(capturedMsg)
-        capMsg.close()
 
         print('created bombe database')
 
@@ -179,73 +148,23 @@ class BombeDatabase:
         self.connection = sqlite3.connect('BombeDB.db')
         print('bombe database connection established')
 
-    # insert message captured into database
-    def insertCapturedMsg(self, msg, day):
+    # insert the correct configuration when it is found by the bombe machine
+    def insertKnownConfig(self, activeR1, offset1, activeR2, offset2, activeR3, offset3, reflector):
         try:
-            # logic to create new message ID
-            # get the largest value message ID that is stored in the database, if there are no messages stored, this query will return null
-            getMsgID = 'SELECT MAX(MessageID) FROM CapturedMsg'
+            # create ID for primary key of db tuple
+            getMaxConfigID = 'SELECT MAX(MessageID) FROM CapturedMsg'
             b = self.connection.cursor()
-            b.execute(getMsgID)
-            msgID = b.fetchone()
+            b.execute(getMaxConfigID)
+            maxID = b.fetchone()
             b.close()
             newID = None
-            # if there are no messages stored in the database, then the message ID will be 1
-            if msgID[0] == None:
+            # if there are no known configurations stored in the database, then the ID will be 1
+            if maxID[0] == None:
                 newID = 1
-            # if there are messages stored in the database, then take the largest value message ID and add 1
+            # if there are known configurations stored in the database, then take the largest value ID and add 1
             else:
-                newID = msgID[0] + 1
-            # insert new captured message into the database
-            insertMessage = 'INSERT INTO CapturedMsg VALUES (' + str(newID) + ', \'' + msg + '\', 0, NULL, ' + str(day) + ');'
-            b = self.connection.cursor()
-            b.execute(insertMessage)
-            b.close()
-            self.connection.commit()
-            print('Message saved.')
-        except sqlite3.OperationalError:
-            print('\nError when trying to insert values, please check the database connection and/or values being input.\n')
-        except sqlite3.IntegrityError:
-            print('\nError when trying to insert values, this configuration already exists in the database.\n')
-
-    # get CapturedMsg from bombe db
-    def getCapturedMsg(self, msgID):
-        # find message in database with matching message id
-        getMessage = 'SELECT * FROM CapturedMsg WHERE MessageID = ' + str(msgID)
-        b = self.connection.cursor()
-        b.execute(getMessage)
-        msgData = b.fetchone()
-        b.close()
-        # if message is not found, print that to the user
-        if msgData == None:
-            returnMsg = 'No message in database with that ID.'
-        else:
-            # if message is not decrypted, just print the encrypted message
-            if msgData[2] == 0:
-                returnMsg = '\nMessage is not decrypted:\nEncrypted message: [' + msgData[1] + ']\n'
-            # if message is decrypted, print both the encryped message and the decrypted message
-            elif msgData[2] == 1:
-                returnMsg = '\nMessage is decrypted:\nEncrypted message: [' + msgData[1] + ']\nDecrypted message: [' + msgData[4] + ']\n'
-        return returnMsg
-
-    # if found, add correct configuration to the possible config table in the bombe db when provided with the correct information
-    def insertPossibleConfig(self, day, activeR1, offset1, activeR2, offset2, reflector):
-        try:
-            insertConfig = 'INSERT INTO PossibleConfig VALUES (' + str(day) + ', ' + str(activeR1) + ', ' + str(offset1) + ', ' + str(activeR2) + ', ' + str(offset2) + ', \'' + reflector + '\');'
-            b = self.connection.cursor()
-            b.execute(insertConfig)
-            b.close()
-            self.connection.commit()
-            print('\nInserting possible config into the Bombe database.\n')
-        except sqlite3.OperationalError:
-            print('\nError when trying to insert values, please check the database connection and/or values being input.\n')
-        except sqlite3.IntegrityError:
-            print('\nError when trying to insert values, this configuration already exists in the database.\n')
-
-    # if found, add correct configuration to the known config table in the bombe db when provided with the correct information
-    def insertKnownConfig(self, day, activeR1, offset1, activeR2, offset2, reflector):
-        try:
-            insertConfig = 'INSERT INTO KnownConfig VALUES (' + str(day) + ', ' + str(activeR1) + ', ' + str(offset1) + ', ' + str(activeR2) + ', ' + str(offset2) + ', \'' + reflector + '\');'
+                newID = maxID[0] + 1
+            insertConfig = 'INSERT INTO KnownConfig VALUES (' + str(newID) + ', ' + str(activeR1) + ', ' + str(offset1) + ', ' + str(activeR2) + ', ' + str(offset2) + ', ' + str(activeR3) + ', ' + str(offset3) + ', \'' + reflector + '\');'
             b = self.connection.cursor()
             b.execute(insertConfig)
             b.close()
@@ -256,41 +175,21 @@ class BombeDatabase:
         except sqlite3.IntegrityError:
             print('\nError when trying to insert values, this configuration already exists in the database.\n')
 
-    # getting known config from bombe db, this is identical to getSchedule() for enigma, but connected to bombe   
-    def getKnownConfig(self, day):
+    # getting known config from bombe db 
+    def getKnownConfig(self, activeR1, offset1, activeR2, offset2, activeR3, offset3, reflector):
         # get all row items in row matching day input by user
-        getConfig = 'SELECT * FROM KnownConfig WHERE Day = ' + str(day)
+        getConfig = 'SELECT * FROM KnownConfig WHERE ActiveR1 = ' + str(activeR1) + ' AND Offset1 = ' + str(offset1) + ' AND ActiveR2 = ' + str(activeR2) + ' AND Offset2 = ' + str(offset2) + ' AND ActiveR3 = ' + str(activeR3) + ' AND Offset3 = ' + str(offset3)
         b = self.connection.cursor()
         b.execute(getConfig)
         knownConfig = b.fetchone()
         b.close()
         # if knownConfig is null, notify the user
-        if not knownConfig:
-            print('No config found for Day ' + str(day))
+        if knownConfig == None:
+            print('No config found for these settings')
             return None
         else:
             # return known enigma configuration
             return knownConfig
-
-    # delete wrong configuration from the bombe db
-    def deleteWrongConfig(self, day, activeR1, offset1, activeR2, offset2, reflector):
-        # find configuration in PossibleConfig
-        findConfig = 'SELECT * FROM PossibleConfig WHERE Day = ' + str(day) + ' AND ActiveR1 = ' + str(activeR1) + ' AND Offset1 = ' + str(offset1) + ' AND ActiveR2 = ' + str(activeR2) + ' AND Offset2 = ' + str(offset2) + ' AND Reflector = \'' + str(reflector) + '\''
-        b = self.connection.cursor()
-        b.execute(findConfig)
-        foundConfig = b.fetchone()
-        b.close()
-        # if query result is null, check inputs/connection
-        if foundConfig == None:
-            print('\nNo result found for this configuration, please check the database connection and/or values being input.\n')
-        # if query result is found, delete the row from the database
-        else:
-            deleteConfig = 'DELETE FROM PossibleConfig WHERE Day = ' + str(day) + ' AND ActiveR1 = ' + str(activeR1) + ' AND Offset1 = ' + str(offset1) + ' AND ActiveR2 = ' + str(activeR2) + ' AND Offset2 = ' + str(offset2) + ' AND Reflector = \'' + reflector + '\''
-            b = self.connection.cursor()
-            b.execute(deleteConfig)
-            b.close()
-            self.connection.commit()
-            print('\nWrong config has been deleted from the possible configs in the database.\n')
 
     # close db connection
     def __del__(self):
@@ -300,3 +199,71 @@ class BombeDatabase:
 # Sources:
 # help with xlsx file support found at https://www.geeksforgeeks.org/python-reading-excel-file-using-openpyxl-module/#:~:text=Openpyxl%20is%20a%20Python%20library,changes%20based%20on%20some%20criteria.
 # destructors in python: https://www.geeksforgeeks.org/destructors-in-python/
+
+###########################################
+##### NOT USING THIS IN THE BETA TEST #####
+###########################################
+# create table script for CapturedMsg table
+# capturedMsg = '''
+# CREATE TABLE CapturedMsg
+# (
+#     MessageID       INT     PRIMARY KEY NOT NULL,
+#     EncryptedMsg    TEXT    NOT NULL,
+#     IsDecrypted     INT     NOT NULL,
+#     DecryptedMsg    TEXT,
+#     Day             INT     NOT NULL
+# );
+# '''        
+# # create CapturedMsg table
+# capMsg = self.bombe.cursor()
+# capMsg.execute(capturedMsg)
+# capMsg.close()
+
+# insert message captured into database
+# def insertCapturedMsg(self, msg, day):
+#     try:
+#         # logic to create new message ID
+#         # get the largest value message ID that is stored in the database, if there are no messages stored, this query will return null
+#         getMsgID = 'SELECT MAX(MessageID) FROM CapturedMsg'
+#         b = self.connection.cursor()
+#         b.execute(getMsgID)
+#         msgID = b.fetchone()
+#         b.close()
+#         newID = None
+#         # if there are no messages stored in the database, then the message ID will be 1
+#         if msgID[0] == None:
+#             newID = 1
+#         # if there are messages stored in the database, then take the largest value message ID and add 1
+#         else:
+#             newID = msgID[0] + 1
+#         # insert new captured message into the database
+#         insertMessage = 'INSERT INTO CapturedMsg VALUES (' + str(newID) + ', \'' + msg + '\', 0, NULL, ' + str(day) + ');'
+#         b = self.connection.cursor()
+#         b.execute(insertMessage)
+#         b.close()
+#         self.connection.commit()
+#         print('Message saved.')
+#     except sqlite3.OperationalError:
+#         print('\nError when trying to insert values, please check the database connection and/or values being input.\n')
+#     except sqlite3.IntegrityError:
+#         print('\nError when trying to insert values, this configuration already exists in the database.\n')
+
+# get CapturedMsg from bombe db
+# def getCapturedMsg(self, msgID):
+#     # find message in database with matching message id
+#     getMessage = 'SELECT * FROM CapturedMsg WHERE MessageID = ' + str(msgID)
+#     b = self.connection.cursor()
+#     b.execute(getMessage)
+#     msgData = b.fetchone()
+#     b.close()
+#     # if message is not found, print that to the user
+#     if msgData == None:
+#         returnMsg = 'No message in database with that ID.'
+#     else:
+#         # if message is not decrypted, just print the encrypted message
+#         if msgData[2] == 0:
+#             returnMsg = '\nMessage is not decrypted:\nEncrypted message: [' + msgData[1] + ']\n'
+#         # if message is decrypted, print both the encryped message and the decrypted message
+#         elif msgData[2] == 1:
+#             returnMsg = '\nMessage is decrypted:\nEncrypted message: [' + msgData[1] + ']\nDecrypted message: [' + msgData[4] + ']\n'
+#     return returnMsg
